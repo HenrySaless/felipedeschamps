22f7f68385f8cd3ff59517bad64addf2e3b636d9
Henry <130256427+HenrySaless@users.noreply.github.com>
2025-06-17 00:30:25 +0000

    change the text of index status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
 function status(request, response) {
-  response.status(200).json({ chave: "valor" });
+  response.status(200).json({ chave: "qulquer coisa aqui dentro" });
 }
 export default status;
